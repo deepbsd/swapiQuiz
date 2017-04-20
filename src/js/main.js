2027d6aae6536000e38ee1410ecc9636d24ac839
David S. Jackson <deepbsd@yahoo.com>
2017-04-20 18:02:52 -0400

    Matthew's promise work...

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,20 @@ var allCharacters = [];
 // swapiModule is from https://github.com/cfjedimaster/SWAPI-Wrapper
 
 
-function getData(page, arr){
+
+// var getData = function(page, arr){
+//   swapiModule.getPeople(page, function(data){
+//     data.results.forEach(function(character){
+//       arr.push(character);
+//       console.log("Name: ", character.name);
+//     });
+//     if (data.next) { getData(page+1, arr); }
+//   })
+//   return arr;
+// }
+
+
+var getData = function(page, arr, callback){
   swapiModule.getPeople(page, function(data){
     data.results.forEach(function(character){
       arr.push(character);
@@ -16,19 +29,26 @@ function getData(page, arr){
   return arr;
 }
 
-new Promise(function(resolve, reject) {
-  var myCharacters = getData(1, allCharacters);
-  myCharacters = resolve;
-  var err_msg = reject;
-})
-.then(function(payload){
-  console.log("Payload is a: ",typeof payload);
-  console.log("HERE'S THE NAME: ", payload[83].name);
-})
-.catch(function(err){
-  console.error("Error: ", err);
-})
 
+const testArr = new Promise((resolve, reject) => {
+  const result = getData(1, allCharacters);
+  resolve(result);
+  //return result;
+}).then(result => console.log(`Result is: ${result[83].name}` ));
+
+// new Promise(function(resolve, reject) {
+//   getData(1, allCharacters);
+//   var myCharacters = resolve;
+//   var err_msg = reject;
+// })
+// .then(function(payload){
+//   console.log("HERE'S THE NAME: ", payload[83].name);
+// })
+// .catch(function(err){
+//   console.log("Error: ", err);
+// });
+
+//getData(1, allCharacters);
 
 // setTimeout( function() {
 //   console.log(allCharacters[83].name);
